d9b24031ee9d925d83e177dbdf7e6ce89e440946
EoinAherne <aherneeoin88@gmail.com>
2022-10-12 14:57:30 +0000

    Add bootstrap cdn,Navbar. Set up template inheritance

--- paradox22/views.py
+++ paradox22/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('products')
+    return HttpResponse('accounts/products.html')
 
 def customer(request):
-    return HttpResponse('customer')
-          
+    return HttpResponse('accounts/customer.html')
+
 
