5aec9da03d9ef8ada0c5549c7484fdb4bfec16fd
EoinAherne <aherneeoin88@gmail.com>
2022-10-17 18:16:40 +0000

    Register Tag model

--- paradox22/admin.py
+++ paradox22/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Customer, Order, Product
+from .models import *
 
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Tag)
 admin.site.register(Order)
+
