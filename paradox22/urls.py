cee3c8b8cef362f39f470a3cb45fe7737a2e7b81
EoinAherne <aherneeoin88@gmail.com>
2022-10-22 14:46:49 +0000

    Remove unneeded views and urls

--- paradox22/urls.py
+++ paradox22/urls.py
@@ -4,10 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('products/', views.products, name="products"),
-    path('customer/<str:pk>/', views.customer, name="customer"),
-    
+        
     path('create_order/', views.createOrder, name="create_order"),
+    
     path('update_order/<str:pk_one>', views.updateOrder, name="update_order"),
     path('delete_order/<str:pk_one>', views.deleteOrder, name="delete_order"),
 
--- paradox22/views.py
+++ paradox22/views.py
@@ -2,38 +2,19 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import OrderForm
-
 # Create your views here.
-
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
-    
-    total_customers = customers.count()
-    total_orders = orders.count()
-    
+        
     delivered = orders.filter(status='Delivered').count()
     pending = orders.filter(status='Pending').count()
     
     context = {'orders' :orders, 'customers':customers,
-    'total_orders': total_orders, 'delivered':delivered, 'pending':pending}
+    'delivered':delivered, 'pending':pending}
 
     return render(request, 'accounts/dashboard.html', context)
 
-def products(request):
-    products = Product.objects.all()
-
-    return HttpResponse('accounts/products.html', {'products': products})
-
-def customer(request, pk):
-
-    customer = Customer.objects.get(id=pk)
-
-    orders = customer.order_set.all()
-    order_count = orders.count()
-
-    context = {'customer':customer, 'orders':orders, 'order_count':order_count}
-    return HttpResponse('accounts/customer.html', context)
 
 #--------------Create view-------->
 def createOrder(request):
